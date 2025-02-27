941aa7b77029eaa51a8bf6457fbddd6f42aab2d6
Sk-gabo <gabriel123ceras12@gmail.com>
2025-02-27 10:51:42 -0500

    primer commit ejercicio1

---
+++ src/main/java/pe/edu/idat/demo_metodos_set/Ejercicio1Controller.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_metodos_set;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Ejercicio1Controller {
+
+    private final Ejercicio1Service ejercicio1Service;
+
+    public Ejercicio1Controller(Ejercicio1Service ejercicio1Service) {
+        this.ejercicio1Service = ejercicio1Service;
+    }
+}
---
+++ src/main/java/pe/edu/idat/demo_metodos_set/Ejercicio1Service.java
@@ -0,0 +1,12 @@
+package pe.edu.idat.demo_metodos_set;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Ejercicio1Service {
+
+    public String SaludoEjercicio(){
+        return  "Hola Git desde mi rama";
+    }
+
+}
