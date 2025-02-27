7752886fd2ce90cf7f7a10299a5d55c0f868a5f0
Sk-gabo <gabriel123ceras12@gmail.com>
2025-02-27 08:53:04 -0500

    iniciar proyecto

---
+++ src/main/java/pe/edu/idat/demo_metodos_set/DemoMetodosSetApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_metodos_set;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMetodosSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMetodosSetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/demo_metodos_set/DemoMetodosSetApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_metodos_set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMetodosSetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
